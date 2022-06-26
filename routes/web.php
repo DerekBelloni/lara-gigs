7c18cce16fa82c3a992786a92fa69a3d7bc6394e
Derek <derekbelloni10@gmail.com>
2022-06-26 10:02:56 -0600

    listing view created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, modi, iusto rerum corporis accusantium enim dignissimos eligendi provident deserunt vero, omnis consequatur dolor eaque tempore.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, modi, iusto rerum corporis accusantium enim dignissimos eligendi provident deserunt vero, omnis consequatur dolor eaque tempore.'
+            ]
+        ]
+    ]);
 });
 
-Route::get('/hello', function () {
-    return response('<h1>A new day has dawned!</h1>');
-});
+// Route::get('/hello', function () {
+//     return response('<h1>A new day has dawned!</h1>', 200)
+//         ->header('Content-Type', 'text/plain');
+// });
+
+// Route::get('/posts/{id}', function ($id) {
+//     return response('Post' . $id);
+// })->where('id', '[0-9]+');
+
+// Route::get('/search', function (Request $request) {
+//     return $request->name . ' ' . $request->city;
+// });
