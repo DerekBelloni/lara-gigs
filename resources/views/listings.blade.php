31b2989f65e8e4b6ff589e03ca615e8ceb979c81
Derek <derekbelloni10@gmail.com>
2022-06-27 13:49:07 -0600

    images folder brought in

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,3 +1,7 @@
+@extends('layout')
+
+@section('content')
+
 <h1>{{$heading}}</h1>
 @unless(count($listings) == 0)
 @foreach($listings as $listing)
@@ -12,3 +16,5 @@
 @else
   <p>No listings found</p>
 @endunless
+
+@endsection
