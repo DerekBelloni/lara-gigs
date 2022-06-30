812a53bb91c4a98a1fd6f32e9a58474e4d81f3ef
Derek <derekbelloni10@gmail.com>
2022-06-30 11:43:57 -0600

    manage listings route created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,23 +32,23 @@ class DatabaseSeeder extends Seeder
             'user_id' => $user->id
         ]);
 
-        Listing::create([
-            'title' => 'Laravel Senior Developer',
-            'tags' => 'laravel javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Boston, MA',
-            'email' => 'email1@email.com',
-            'website' => 'https://www.acme.com',
-            'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe vero laborum unde provident sunt excepturi, quibusdam placeat facilis fugit. Deserunt aliquid, quisquam blanditiis molestiae quam doloremque! Unde architecto explicabo aspernatur voluptatibus quos, maxime veniam quidem. Hic aliquid itaque veritatis! Eaque cum ullam accusamus corporis dolorum. Illum quod delectus cum vitae?'
-        ]);
-        Listing::create([
-            'title' => 'Full-Stack Engineer',
-            'tags' => 'laravel, backend, api',
-            'company' => 'Stark Industries',
-            'location' => 'New York, NY',
-            'email' => 'email2@email.com',
-            'website' => 'https://www.starkindustries.com',
-            'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe vero laborum unde provident sunt excepturi, quibusdam placeat facilis fugit. Deserunt aliquid, quisquam blanditiis molestiae quam doloremque! Unde architecto explicabo aspernatur voluptatibus quos, maxime veniam quidem. Hic aliquid itaque veritatis! Eaque cum ullam accusamus corporis dolorum. Illum quod delectus cum vitae?'
-        ]);
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe vero laborum unde provident sunt excepturi, quibusdam placeat facilis fugit. Deserunt aliquid, quisquam blanditiis molestiae quam doloremque! Unde architecto explicabo aspernatur voluptatibus quos, maxime veniam quidem. Hic aliquid itaque veritatis! Eaque cum ullam accusamus corporis dolorum. Illum quod delectus cum vitae?'
+        // ]);
+        // Listing::create([
+        //     'title' => 'Full-Stack Engineer',
+        //     'tags' => 'laravel, backend, api',
+        //     'company' => 'Stark Industries',
+        //     'location' => 'New York, NY',
+        //     'email' => 'email2@email.com',
+        //     'website' => 'https://www.starkindustries.com',
+        //     'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe vero laborum unde provident sunt excepturi, quibusdam placeat facilis fugit. Deserunt aliquid, quisquam blanditiis molestiae quam doloremque! Unde architecto explicabo aspernatur voluptatibus quos, maxime veniam quidem. Hic aliquid itaque veritatis! Eaque cum ullam accusamus corporis dolorum. Illum quod delectus cum vitae?'
+        // ]);
     }
 }
